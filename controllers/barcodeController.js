604493479f4ea30cd39bfd6c3140459fef8ef7f6
Diana Fisher <diana.fisher@gmail.com>
2017-05-26 17:29:50 -0700

    Return JSON from barcode lookup endpoint

--- controllers/barcodeController.js
+++ controllers/barcodeController.js
@@ -16,8 +16,10 @@ exports.lookupBarCode = (req, res) => {
     console.log('statusCode: ', response.statusCode);
     console.log('headers: ', response.headers);
     response.on('data', function(d) {
-      res.send(`Looking up code ${code}`);
       console.log('BODY: ' + d);
+      console.log(d.toString());
+      const data = d.toString();
+      res.json(data);
     })
   })
   request.on('error', function(e) {
